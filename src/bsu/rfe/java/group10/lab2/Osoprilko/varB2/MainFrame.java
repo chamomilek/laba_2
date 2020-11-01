fd4114d4390119133f81111a839ea0f5a2a10047
Karina Osoprilko <chamomileekk@gmail.com>
2020-11-01 21:37:20 +0300

    Start

---
+++ src/bsu/rfe/java/group10/lab2/Osoprilko/varB2/MainFrame.java
@@ -0,0 +1,62 @@
+package bsu.rfe.java.group10.lab2.Osoprilko.varB2;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+
+
+
+public class MainFrame extends JFrame {
+    // Размеры окна приложения в виде констант
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 320;
+    // Текстовые поля для считывания значений переменных X и Y,
+// как компоненты, совместно используемые в различных методах
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+    private JTextField textFieldZ;
+    private JTextField textFieldMem1;
+    private JTextField textFieldMem2;
+    private JTextField textFieldMem3;
+    private JTextField textFieldResult;
+    // Группа радио-кнопок для обеспечения уникальности выделения в группе
+    private ButtonGroup radioButtons = new ButtonGroup();
+    private ButtonGroup radioMemButtons = new ButtonGroup();
+    // Контейнер для отображения радио-кнопок
+    private Box hboxFormulaType = Box.createHorizontalBox();
+    // Идентификатор выбранной формулы
+    private int formulaId = 1;
+    private Box hboxMemType = Box.createHorizontalBox();
+    private int MemId = 1;
+    // Формула №1 для рассчѐта
+    public Double calculate1(Double x, Double y) {
+        return x*x + y*y;
+    }
+    // Формула №2 для рассчѐта
+    public Double calculate2(Double x, Double y) {
+        return x*x*x + 1/y;
+    }
+
+
+}
+
+    public static void main(String[] args) {
+        MainFrame frame= new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
